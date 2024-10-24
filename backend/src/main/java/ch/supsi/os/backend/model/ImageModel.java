460892a07f5725ea0cc171286fa6c5d62c6065b4
Lorusso Marco <marco.lorusso@student.supsi.ch>
2024-10-24 11:18:08 +0200

    implemented side to side image flip

--- backend/src/main/java/ch/supsi/os/backend/model/ImageModel.java
+++ backend/src/main/java/ch/supsi/os/backend/model/ImageModel.java
@@ -90,4 +90,25 @@ public class ImageModel {
         }
         pixels = flippedPixels;
     }
+    public void flipImageSideToSide() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width / 2; j++) {
+                if (channels == 3) {
+                    int leftIndex = j * 3;
+                    int rightIndex = (width - 1 - j) * 3;
+
+                    for (int k = 0; k < 3; k++) {
+                        int temp = pixels[i][leftIndex + k];
+                        pixels[i][leftIndex + k] = pixels[i][rightIndex + k];
+                        pixels[i][rightIndex + k] = temp;
+                    }
+                } else {
+                    int temp = pixels[i][j];
+                    pixels[i][j] = pixels[i][width - 1 - j];
+                    pixels[i][width - 1 - j] = temp;
+                }
+            }
+        }
+    }
+
 }
