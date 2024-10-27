888f59d6589b0cc841771d5ecb13936e78e73602
adrianochiriaco13 <129774373+adrianochiriaco13@users.noreply.github.com>
2024-10-27 20:40:34 +0100

    feature:25 negative image trasformation

--- backend/src/test/java/ch/supsi/os/backend/model/ImageModelTest.java
+++ backend/src/test/java/ch/supsi/os/backend/model/ImageModelTest.java
@@ -189,6 +189,50 @@ public class ImageModelTest {
         assertArrayEquals(expectedPixels, imageModel.getPixels(), "The RGB image should be rotated 90 degrees anti-clockwise");
     }
 
+    @Test
+    public void testNegativeTransformationGrayscale() {
+        int[][] originalPixels = {
+                {0},   // Black
+                {128}, // Mid gray
+                {255}  // White
+        };
+
+        ImageModel imageModel = new ImageModel("P2", 1, 3, originalPixels, 1);
+        imageModel.negativeTransformation();
+
+        int[][] expectedPixels = {
+                {255}, // Negative of black
+                {127}, // Negative of mid gray
+                {0}    // Negative of white
+        };
+
+        assertArrayEquals(expectedPixels, imageModel.getPixels(), "The grayscale image should be transformed to negative correctly");
+    }
+
+    @Test
+    public void testNegativeTransformationRGB() {
+        int[][] originalPixels = {
+                {255, 0, 0},   // Red
+                {0, 255, 0},   // Green
+                {0, 0, 255},   // Blue
+                {255, 255, 255}, // White
+                {0, 0, 0}      // Black
+        };
+
+        ImageModel imageModel = new ImageModel("P3", 3, 5, originalPixels, 3);
+        imageModel.negativeTransformation();
+
+        int[][] expectedPixels = {
+                {0, 255, 255},   // Negative of red
+                {255, 0, 255},   // Negative of green
+                {255, 255, 0},   // Negative of blue
+                {0, 0, 0},       // Negative of white
+                {255, 255, 255}  // Negative of black
+        };
+
+        assertArrayEquals(expectedPixels, imageModel.getPixels(), "The RGB image should be transformed to negative correctly");
+    }
+
 
 
 }
